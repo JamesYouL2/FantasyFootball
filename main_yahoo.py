bfd43551d4c15d2c7d3c263889ed807e50a8744a
James You <james.jy.you@gmail.com>
2024-11-19 13:14:15 -0800

    remove notebook in favor of py script

---
+++ main_yahoo.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from roster import Roster
+from tradevalue import TradeValue
+
+#change leagueid here
+
+def col_replace(df, column_name):
+    df[column_name] = df[column_name].str.replace('.', '', regex=False)
+    df[column_name] = df[column_name].str.replace('Kenneth', 'Ken', regex=False)
+    df[column_name] = df[column_name].str.replace(' Jr', '', regex=False)
+    df[column_name] = df[column_name].str.rstrip(' III')
+    df[column_name] = df[column_name].str.rstrip(' II')
+    df[column_name] = df[column_name].str.replace(' Sr', '', regex=False)
+    return df
+
+def df_to_csv(leagueid):
+    team1 = Roster(leagueid=leagueid)
+    TradeValue_Class = TradeValue("https://www.thescore.com/nfl/news/3133428/fantasy-trade-value-chart-week-12")
+    TradeValue_df = col_replace(TradeValue_Class.all_dfs, 'Player')
+    team1_df = col_replace(team1.roster_df, 'player_name')
+    all_df = pd.merge(TradeValue_df,team1_df,left_on='Player',right_on='player_name',how='left')
+    all_df.sort_values('Value', ascending=False).to_csv(f'{leagueid}.csv', sep=',')
+    return all_df
+
+if __name__ == "__main__":
+    main(55598)
+    main(55617)
